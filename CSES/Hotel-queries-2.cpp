ae1857fb70c0c533fc01dac2fd8c3c3d063baf7c
AndreiDani <89001893+AndreiDaniDev@users.noreply.github.com>
2025-11-17 18:34:11 +0200

    Update and rename Hotel-Queries-2.cpp to Hotel-queries-2.cpp

--- CSES/Hotel-queries-2.cpp
+++ CSES/Hotel-queries-2.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #define in cin
 #define out cout
-
+ 
 using namespace std;
-
+ 
 const int nmax = 2e5;
 int n, nrq, a[nmax + 2], value, wherey;
-
+ 
 struct myaint{
     int tree[4 * nmax + 2];
-
+ 
     void build(int node, int st, int dr){
         if(st == dr){
             tree[node] = a[st];
@@ -20,7 +20,7 @@ struct myaint{
             tree[node] = max(tree[(node << 1)], tree[(node << 1) | 1]);
         }
     }
-
+ 
     void update(int node, int st, int dr, int idx){
         if(st == dr){
             tree[node] = a[st];
@@ -31,7 +31,7 @@ struct myaint{
             tree[node] = max(tree[(node << 1)], tree[(node << 1) | 1]);
         }
     }
-
+ 
     int query(int node, int st, int dr, int value){
         if(st == dr){
             return st;
@@ -43,25 +43,27 @@ struct myaint{
         }
     }
 } myaint;
-
+ 
 int main(){
-
+ 
+    in.tie(NULL) -> sync_with_stdio(false); out.tie(NULL);
+ 
     in>>n>>nrq;
     for(int i = 1; i <= n; i++)
         in>>a[i];
-
+ 
     myaint.build(1, 1, n);
-
+ 
     for(int itq = 1; itq <= nrq; itq++){
         in>>value;
-
+ 
         if(myaint.tree[1] < value){ out<<"0\n"; continue; }
-
+ 
         wherey = myaint.query(1, 1, n, value);
         a[wherey] -= value; myaint.update(1, 1, n, wherey);
-
+ 
         out<<wherey<<" ";
     }; out<<"\n";
-
+ 
     return 0;
 }
